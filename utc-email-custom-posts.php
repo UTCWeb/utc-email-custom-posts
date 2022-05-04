a34998f9cfff7e1fb591951b2730f5813a938cd3
Chris Gilligan <chris-gilligan@utc.edu>
2022-05-04 14:14:55 -0400

    Initial add, all files.
    Signed-off-by: Chris Gilligan <chris-gilligan@utc.edu>

---
+++ utc-email-custom-posts.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Utc Email Custom Posts
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     utc-email-custom-posts
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Utc_Email_Custom_Posts
+ */
+
+// Your code starts here.
