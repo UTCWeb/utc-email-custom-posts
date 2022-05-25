21fb236bf761b398c8c5ffb611e9bbc8c15e89bf
Chris Gilligan <chris-gilligan@utc.edu>
2022-05-25 10:02:43 -0400

    UTCE: Bump version for release.
    Signed-off-by: Chris Gilligan <chris-gilligan@utc.edu>

--- utc-email-custom-posts.php
+++ utc-email-custom-posts.php
@@ -7,7 +7,7 @@
  * Author URI:      https://chrisgilligan.com/
  * Text Domain:     utc-email-custom-posts
  * Domain Path:     /languages
- * Version:         0.2.2
+ * Version:         0.2.3
  *
  * @package         Utc_Email_Custom_Posts
  */
