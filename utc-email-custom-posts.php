4cf7e4ca44e61491a94fd9cdf4f1299ddba67bfb
Chris Gilligan <chris-gilligan@utc.edu>
2022-06-09 11:11:32 -0400

    UTCE: Responsive image captions.
    Signed-off-by: Chris Gilligan <chris-gilligan@utc.edu>
    
    Removed height/width attributes from caption shortcode.

--- utc-email-custom-posts.php
+++ utc-email-custom-posts.php
@@ -201,3 +201,29 @@ function remove_thumbnail_dimensions( $html ) {
     $html = preg_replace( '/(width|height)=\"\d*\"\s/', 'style="max-width: 100%;"', $html );
     return $html;
 }
+// Responsive image captions: remove height/width attributes
+add_shortcode( 'wp_caption', 'fixed_img_caption_shortcode' );
+add_shortcode( 'caption', 'fixed_img_caption_shortcode' );
+
+function fixed_img_caption_shortcode($attr, $content = null) {
+    if ( ! isset( $attr['caption'] ) ) {
+        if ( preg_match( '#((?:<a [^>]+>\s*)?<img [^>]+>(?:\s*</a>)?)(.*)#is', $content, $matches ) ) {
+            $content = $matches[1];
+            $attr['caption'] = trim( $matches[2] );
+        }
+    }
+    $output = apply_filters( 'img_caption_shortcode', '', $attr, $content );
+    if ( $output != '' )
+        return $output;
+    extract( shortcode_atts(array(
+        'id'      => '',
+        'align'   => 'alignnone',
+        'width'   => '',
+        'caption' => ''
+    ), $attr));
+    if ( 1 > (int) $width || empty($caption) )
+        return $content;
+    if ( $id ) $id = 'id="' . esc_attr($id) . '" ';
+    return '<div ' . $id . 'class="wp-caption ' . esc_attr($align) . '" >'
+        . do_shortcode( $content ) . '<p class="wp-caption-text">' . $caption . '</p></div>';
+}
